74e3231bde9aec82cae0188213f9f6005798a742
rinakarnauch <rina.karnauch@mail.huji.ac.il>
2022-09-29 16:34:34 +0300

    title is kind of ready except theme

--- src/Components/HeaderComponents/Title.js
+++ src/Components/HeaderComponents/Title.js
@@ -1,6 +1,8 @@
 import classes from './Title.module.css'
-import {FaAngleDoubleDown, FaAngleDoubleUp} from 'react-icons/fa';
+import {FaAngleDoubleDown, FaAngleDoubleUp, FaFacebook} from 'react-icons/fa';
 import {useState} from "react";
+import Typography from "@mui/material/Typography";
+import {GiHeraldicSun, GiNightSky} from "react-icons/gi";
 
 const Title = (props) => {
 
@@ -17,6 +19,20 @@ const Title = (props) => {
             <button id={classes['dropdown-button']} onClick={handleNavClick}>
                 {isDown ? <FaAngleDoubleDown size={30}/> : <FaAngleDoubleUp size={30}/>}
             </button>
+            <Typography id={classes['title']} variant="h3">Huji Confessions</Typography>
+            <div id={classes['header-buttons-container']}>
+                <button className={classes['header-buttons']}>
+                        <GiNightSky id={classes['button-mode']}
+                                    className={classes['button-center']}
+                        />
+                </button>
+                <button className={classes['header-buttons']}>
+                    <FaFacebook
+                        id={classes['button-facebook']}
+                        className={classes['button-center']}
+                    />
+                </button>
+            </div>
         </div>
 
     );
